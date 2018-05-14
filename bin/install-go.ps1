1ae413b7c6bfc5aef587524f72d2c861bcab79f4
Michael Xu <mxu@pivotal.io>
2018-05-14 11:25:02 -0700

    Use latest golang for windows unit tests
    [#156582073](https://www.pivotaltracker.com/story/show/156582073)
    
    Co-authored-by: Danny Berger <dberger@pivotal.io>

---
+++ bin/install-go.ps1
@@ -0,0 +1,31 @@
+trap {
+  write-error $_
+  exit 1
+}
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest 'https://github.com/stedolan/jq/releases/download/jq-1.5/jq-win64.exe' -OutFile jq.exe
+Invoke-WebRequest 'https://golang.org/dl/?mode=json' -OutFile golang.json
+
+$GO_VERSION = $(./jq.exe -r '.[0].files[] | select(.os == """windows""" and .arch == """amd64""" and .kind == """installer""").version' ./golang.json)
+
+Write-Host "Checking if Go needs to be installed or updated..."
+if ((Get-Command 'go.exe' -ErrorAction SilentlyContinue) -ne $null) {
+  $version = "$(go.exe version)"
+  if ($version -match "go version $GO_VERSION windows\/amd64") {
+    Write-Host "Golang $GO_VERSION already installed, skipping download."
+    exit 0
+  }
+}
+
+Write-Host "Installing $GO_VERSION"
+
+Invoke-WebRequest "https://storage.googleapis.com/golang/$GO_VERSION.windows-amd64.msi" ` -UseBasicParsing -OutFile go.msi
+
+$p = Start-Process -FilePath "msiexec" ` -ArgumentList "/passive /norestart /i go.msi" ` -Wait -PassThru
+
+if ($p.ExitCode -ne 0) {
+  throw "Golang MSI installation process returned error code: $($p.ExitCode)"
+}
+
+Write-Host "Successfully installed go version: $(go version)"
--- ci/tasks/test-unit.ps1
+++ ci/tasks/test-unit.ps1
@@ -9,36 +9,7 @@ $env:PATH = $env:GOPATH + "/bin;C:/go/bin;C:/var/vcap/bosh/bin;" + $env:PATH
 
 cd $env:GOPATH/src/github.com/cloudfoundry/gosigar
 
-function NeedsToInstallGo() {
-    Write-Host "Checking if Go needs to be installed or updated..."
-    if ((Get-Command 'go.exe' -ErrorAction SilentlyContinue) -eq $null) {
-        Write-Host "Go.exe not found, Go will be installed"
-        return $true
-    }
-    $version = "$(go.exe version)"
-    if ($version -match 'go version go1\.[1-7]\.\d windows\/amd64') {
-        Write-Host "Installed version of Go is not supported, Go will be updated"
-        return $true
-    }
-    Write-Host "Found Go version '$version' installed on the system, skipping install"
-    return $false
-}
-
-if (NeedsToInstallGo) {
-    Write-Host "Installing Go 1.8.3"
-
-    Invoke-WebRequest 'https://storage.googleapis.com/golang/go1.8.3.windows-amd64.msi' `
-        -UseBasicParsing -OutFile go.msi
-
-    $p = Start-Process -FilePath "msiexec" `
-        -ArgumentList "/passive /norestart /i go.msi" `
-        -Wait -PassThru
-    if ($p.ExitCode -ne 0) {
-        throw "Golang MSI installation process returned error code: $($p.ExitCode)"
-    }
-
-    Write-Host "Successfully installed go version: $(go version)"
-}
+powershell.exe bin/install-go.ps1
 
 go.exe install github.com/cloudfoundry/gosigar/vendor/github.com/onsi/ginkgo/ginkgo
 if ($LASTEXITCODE -ne 0) {
