0a694e99f7dd97606ca3fe06c29a4fc57163c16a
Luan Santos <lsantos@pivotal.io>
2018-05-25 16:04:08 -0700

    Lock golang version to 1.x
    [#156582099](https://www.pivotaltracker.com/story/show/156582099)
    
    Co-authored-by: Danny Berger <dberger@pivotal.io>

--- bin/install-go.ps1
+++ bin/install-go.ps1
@@ -7,7 +7,7 @@ trap {
 Invoke-WebRequest 'https://github.com/stedolan/jq/releases/download/jq-1.5/jq-win64.exe' -OutFile jq.exe
 Invoke-WebRequest 'https://golang.org/dl/?mode=json' -OutFile golang.json
 
-$GO_VERSION = $(./jq.exe -r '.[0].files[] | select(.os == """windows""" and .arch == """amd64""" and .kind == """installer""").version' ./golang.json)
+$GO_VERSION = $(./jq.exe -r 'map(select(.stable and (.version | split(""".""")[0] == """go1"""))) | .[0].files[] | select(.os == """windows""" and .arch == """amd64""" and .kind == """installer""").version' ./golang.json)
 
 Write-Host "Checking if Go needs to be installed or updated..."
 if ((Get-Command 'go.exe' -ErrorAction SilentlyContinue) -ne $null) {
